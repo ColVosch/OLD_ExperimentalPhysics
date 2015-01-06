862a97b81aad75f5f780375313325bcec42faa2b
ColVosch <snowghost@gazeta.pl>
2015-01-06 20:00:48 +0100

    made the AdvancedRefinerDisplay usable

--- src/main/java/experimentalPhysics/tileEntitys/TileEntityAdvancedRefinerDisplay.java
+++ src/main/java/experimentalPhysics/tileEntitys/TileEntityAdvancedRefinerDisplay.java
@@ -1,7 +1,8 @@
 package experimentalPhysics.tileEntitys;
 
-import net.minecraft.tileentity.TileEntity;
 import experimentalPhysics.util.Position;
+
+import net.minecraft.tileentity.TileEntity;
 
 public class TileEntityAdvancedRefinerDisplay extends TileEntity
 {
@@ -9,14 +10,44 @@ public class TileEntityAdvancedRefinerDisplay extends TileEntity
 	
 	private Position refinerPos;
 	
-	public void setRefinerPos(Position refinerPos)
+	public void form(Position refinerPos)
 	{
 		this.refinerPos = refinerPos;
 	}
 
+	public boolean hasRefiner()
+	{
+		return refinerPos != null;
+	}
+	
 	public TileEntityAdvancedRefiner getRefiner()
 	{
-		return (TileEntityAdvancedRefiner) refinerPos.getTileEntity(worldObj);
+		return hasRefiner() ? (TileEntityAdvancedRefiner) refinerPos.getTileEntity(worldObj) : null;
+	}
+
+	public float getProgressPercentage()
+	{
+		TileEntityAdvancedRefiner refiner = getRefiner();
+		if (hasRefiner())
+		{
+			if (refiner.getProgress() == -1)
+			{
+				return 0;
+			}
+			else
+			{
+				return  (float) refiner.getProgress() / (float) TileEntityAdvancedRefiner.REQUIRED_PROGRESS * 100f;
+			}
+		}
+		else
+		{
+			return -1;
+		}
+	}
+
+	public float getHeat()
+	{
+		return hasRefiner() ? getRefiner().getHeat() : -1;
 	}
 	
 }
